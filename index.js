dda47d7d295592a4b97cb0e9f7dfdd4bb12017b3
Kirstie <kirstiewithrow@gmail.com>
2021-09-26 23:43:34 -0500

    Completed 9.26

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let num1 = 31;
 let num2 = 2;
 const multiply = num1 * num2;
-const random = Math.random();
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 16;
+let num4 = 4;
+const mod = num3 / num4;
+let max = Math.max(5,10,15,20);
